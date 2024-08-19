aa5a01bdb592a416fd009875dd8adff947c609e7
Marcus Gamboa <marcus.gamboa@gmail.com>
2024-08-19 15:55:09 -0600

    Add Sanity content to slug page

--- src/app/(client)/posts/[slug]/page.tsx
+++ src/app/(client)/posts/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { client } from "@/sanity/lib/client";
 import { PortableText } from "next-sanity";
+import { urlFor } from "@/sanity/lib/image";
+import Image from "next/image";
 
 const getPost = async (slug: string) => {
   const query = `*[_type == "post" && slug.current == "${slug}"][0] {
@@ -9,6 +11,7 @@ const getPost = async (slug: string) => {
     publishedAt,
     excerpt,
     content,
+    titleImage,
     _id
 }
   `;
@@ -18,13 +21,19 @@ const getPost = async (slug: string) => {
 };
 
 export default async function page({ params }: { params: { slug: string } }) {
-  console.log(params);
   const post = await getPost(params.slug);
-  console.log(post);
+
   return (
-    <div>
-      {params.slug}
-      <div>
+    <div className="mt-8 flex flex-col items-center justify-center px-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <h1>
+        <span className="block text-base text-center text-blue-500 font-semibold tracking-wide uppercase">Adri Morrs - Blog</span>
+        {/* <span className="block text-base text-center text-primary font-semibold tracking-wide uppercase">Adri Morrs - Blog</span> */}
+        <span className="mt-2 block text-3xl text-center leading-8 font-bold tracking-tight sm:text-4xl">{post.title}</span>
+      </h1>
+
+      <Image src={urlFor(post.titleImage).url()} width={400} height={400} alt="Title Image" priority className="rounded-lg mt-8 border" />
+
+      <div className="container mt-16 prose prose-blue prose-lg dark:prose-invert prose-li:marker:text-primary prose-a:text-primary bg-red-500">
         <PortableText value={post.content} />
       </div>
     </div>
