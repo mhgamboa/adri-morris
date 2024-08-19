cf9640d891982acaba2336845179d9aca8625f05
Marcus Gamboa <marcus.gamboa@gmail.com>
2024-08-19 13:22:44 -0600

    Update Post Schema

--- src/sanity/schemaTypes/PostType.ts
+++ src/sanity/schemaTypes/PostType.ts
@@ -1,12 +1,26 @@
-export const post = {
+import { defineType, Rule } from "sanity";
+
+export const post = defineType({
   name: "post",
   title: "Post",
   type: "document",
   fields: [
-    { name: "title", title: "Title", type: "string" },
-    { name: "slug", title: "Slug", type: "slug", options: { source: "title", maxLength: 96 } },
-    { name: "publishedAt", title: "Published at", type: "datetime" },
+    { name: "title", title: "Title", type: "string", validation: (Rule: Rule) => Rule.required().error("Required") },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "title", maxLength: 200 },
+      validation: (Rule: Rule) => Rule.required().error("Max 200 characters"),
+    },
+    { name: "titleImage", title: "Title image", type: "image" },
+    { name: "publishedAt", title: "Published at", type: "datetime", initialValue: new Date().toISOString() },
     { name: "excerpt", title: "Excerpt", type: "text", rows: 3 },
-    { name: "content", title: "Content", type: "array", of: [{ type: "block" }] },
+    {
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [{ type: "block" }, { type: "image", fields: [{ type: "text", name: "alt", title: "Alt" }] }],
+    },
   ],
-};
+});
