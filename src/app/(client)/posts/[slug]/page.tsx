2cbae9f1df4f4827c8bbcbda51cc9f7282efb21d
Marcus Gamboa <marcus.gamboa@gmail.com>
2024-08-19 15:19:55 -0600

    Add Blog content from Next-Sanity using PortableText

--- src/app/(client)/posts/[slug]/page.tsx
+++ src/app/(client)/posts/[slug]/page.tsx
@@ -1,5 +1,32 @@
 import React from "react";
+import { client } from "@/sanity/lib/client";
+import { PortableText } from "next-sanity";
 
-export default async function page() {
-  return <div>page</div>;
+const getPost = async (slug: string) => {
+  const query = `*[_type == "post" && slug.current == "${slug}"][0] {
+    title,
+    slug,
+    publishedAt,
+    excerpt,
+    content,
+    _id
+}
+  `;
+
+  const post = client.fetch(query);
+  return post;
+};
+
+export default async function page({ params }: { params: { slug: string } }) {
+  console.log(params);
+  const post = await getPost(params.slug);
+  console.log(post);
+  return (
+    <div>
+      {params.slug}
+      <div>
+        <PortableText value={post.content} />
+      </div>
+    </div>
+  );
 }
