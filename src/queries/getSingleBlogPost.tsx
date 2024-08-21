39bc23c7df25da99912140c77d0331638d5adccf
Marcus Gamboa <marcus.gamboa@gmail.com>
2024-08-21 14:23:04 -0600

    Create getSingleBlogPost.tsx

---
+++ src/queries/getSingleBlogPost.tsx
@@ -0,0 +1,24 @@
+import "server-only";
+import { client } from "@/sanity/lib/client";
+import { redirect } from "next/navigation";
+
+export default async function getSingleBlogPost(slug: string) {
+  const query = `*[_type == "blogPost" && slug.current == "${slug}"][0] {
+    title,
+    slug,
+    publishedAt,
+    excerpt,
+    content,
+    titleImage,
+    _id
+    }`;
+
+  try {
+    const blogPost = await client.fetch(query);
+    if (!blogPost) redirect("/error");
+    return blogPost;
+  } catch (error) {
+    console.log(error);
+    redirect("/error");
+  }
+}
