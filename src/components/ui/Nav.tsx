9de493c52f06c41027f250150bfd87ebe276a72f
Marcus Gamboa <marcus.gamboa@gmail.com>
2024-08-21 11:42:16 -0600

    Update Nav tags. a ➡️Link. Add About Path

--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -1,35 +1,40 @@
 "use client";
 import { useState, useRef, useEffect } from "react";
+import Link from "next/link";
 import Image from "next/image";
 
 export default function Nav() {
   const [menuState, setMenuState] = useState(false);
 
   const navigation = [
-    { title: "Customers", path: "#" },
-    { title: "Careers", path: "#" },
-    { title: "Guides", path: "#" },
-    { title: "Partners", path: "#" },
+    { title: "Blog", path: "#" },
+    { title: "About", path: "/about" },
+    // { title: "Guides", path: "#" },
+    // { title: "Partners", path: "#" },
   ];
   return (
     <nav className="bg-white border-b">
       <div className="flex items-center space-x-8 py-3 px-4 max-w-screen-xl mx-auto md:px-8">
         <div className="flex-none lg:flex-initial">
-          <a href="#">{/* <Image src="https://www.floatui.com/logo.svg" width={120} height={50} alt="Float UI logo" /> */}</a>
+          <Link href="/">
+            <Image src="https://www.floatui.com/logo.svg" width={120} height={50} alt="Float UI logo" />
+          </Link>
+          {/* <a href="#">
+            <Image src="https://www.floatui.com/logo.svg" width={120} height={50} alt="Float UI logo" />
+          </a> */}
         </div>
         <div className="flex-1 flex items-center justify-between">
           <div className={`bg-white absolute z-20 w-full top-16 left-0 p-4 border-b lg:static lg:block lg:border-none ${menuState ? "" : "hidden"}`}>
             <ul className="mt-12 space-y-5 lg:flex lg:space-x-6 lg:space-y-0 lg:mt-0">
-              {navigation.map((item, idx) => (
-                <li key={idx} className="text-gray-600 hover:text-gray-900">
-                  <a href={item.path}>{item.title}</a>
+              {navigation.map(item => (
+                <li key={item.title} className="text-gray-600 hover:text-gray-900">
+                  <Link href={item.path}>{item.title}</Link>
                 </li>
               ))}
             </ul>
-            {/* <ProfileDropDown class="mt-5 pt-5 border-t lg:hidden" /> */}
           </div>
           <div className="flex-1 flex items-center justify-end space-x-2 sm:space-x-6">
-            <form className="flex items-center space-x-2 border rounded-md p-2">
+            {/* <form className="flex items-center space-x-2 border rounded-md p-2">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5 flex-none text-gray-300"
@@ -40,7 +45,7 @@ export default function Nav() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
               </svg>
               <input className="w-full outline-none appearance-none placeholder-gray-500 text-gray-500 sm:w-auto" type="text" placeholder="Search" />
-            </form>
+            </form> */}
             {/* <ProfileDropDown class="hidden lg:block" /> */}
             <button className="outline-none text-gray-400 block lg:hidden" onClick={() => setMenuState(!menuState)}>
               {menuState ? (
